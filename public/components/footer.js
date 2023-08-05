71185872342e656486dfa44c5629060c70db9233
yuden007 <lyhyuden007@gmail.com>
2023-08-05 16:32:31 +0800

    Create individual file for header and footer

---
+++ public/components/footer.js
@@ -0,0 +1,29 @@
+const footerContent = `
+    <footer class="text-darkgray bg-lightorange font-body">
+    <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
+        <a class="flex font-title font-medium items-center md:justify-start justify-center text-gray-900">
+            <img src="img/MSA Logo Transparent.png" class="w-12" alt="MSA logo">
+            <span class="ml-3 text-xl">MSACUHK</span>
+        </a>
+        <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-darkgray sm:py-2 sm:mt-0 mt-4">©
+            2020 MSACUHK</p>
+        <span class="inline-flex sm:ml-auto sm:mt-0 mt-4 justify-center sm:justify-start">
+            <a href="https://www.facebook.com/msacuhk/" class="text-gray-500" rel="noopener noreferrer"
+                target="_blank">
+                <svg fill="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                    class="w-5 h-5" viewBox="0 0 24 24">
+                    <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
+                </svg>
+            </a>
+            <a href="https://www.instagram.com/msacuhk" class="ml-3 text-gray-500" rel="noopener noreferrer"
+                target="_blank">
+                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round"
+                    stroke-width="2" class="w-5 h-5" viewBox="0 0 24 24">
+                    <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+                    <path d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"></path>
+                </svg>
+            </a>
+        </span>
+    </div>
+    </footer>
+`
